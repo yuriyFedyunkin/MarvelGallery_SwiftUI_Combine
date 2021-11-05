df5cc328319df157163b2dea1eb9196ca03d16ad
Yuriy Fedyunkin <samnx39@gmail.com>
2021-11-05 16:16:21 +0200

    git ignore setup

---
+++ MarvelGallery_SwiftUI_Combine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelGallery_SwiftUI_Combine
+//
+//  Created by Yuriy Fedyunkin on 05.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelGallery_SwiftUI_Combine/MarvelGallery_SwiftUI_CombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelGallery_SwiftUI_CombineApp.swift
+//  MarvelGallery_SwiftUI_Combine
+//
+//  Created by Yuriy Fedyunkin on 05.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MarvelGallery_SwiftUI_CombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
